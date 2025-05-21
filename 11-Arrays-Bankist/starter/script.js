0accee5bcae08dd56acdc8f3eb5f5c14edc88a49
DavideSchirru17 <schirrudavide@icloud.com>
2025-05-21 19:49:30 +0100

    feat: Object oriented Programming

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -85,6 +85,13 @@ const displayMovements = function (movements, sort = false) {
 
 // invoke the function
 displayMovements(account1.movements);
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -497,3 +504,110 @@ const deposit = mov => mov > 0;
 console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
+console.log('-----challenge #4 ----------');
+// challenge 4
+/*
+This time, Julia and Kate are studying the activity levels of different dog breeds.
+
+YOUR TASKS:
+1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+3. Create an array "allActivities" of all the activities of all the dog breeds
+4. Create an array "uniqueActivities" that contains only the unique activities (no activity repetitions). HINT: Use a technique with a special data structure that we studied a few sections ago.
+5. Many dog breeds like to swim. What other activities do these dogs like? Store all the OTHER activities these breeds like to do, in a unique array called "swimmingAdjacent".
+6. Do all the breeds have an average weight of 10kg or more? Log to the console whether "true" or "false".
+7. Are there any breeds that are "active"? "Active" means that the dog has 3 or more activities. Log to the console whether "true" or "false".
+
+BONUS: What's the average weight of the heaviest breed that likes to fetch? HINT: Use the "Math.max" method along with the ... operator.
+
+TEST DATA:
+*/
+
+const breeds = [
+  {
+    breed: 'German Shepherd',
+    averageWeight: 32,
+    activities: ['fetch', 'swimming'],
+  },
+  {
+    breed: 'Dalmatian',
+    averageWeight: 24,
+    activities: ['running', 'fetch', 'agility'],
+  },
+  {
+    breed: 'Labrador',
+    averageWeight: 28,
+    activities: ['swimming', 'fetch'],
+  },
+  {
+    breed: 'Beagle',
+    averageWeight: 12,
+    activities: ['digging', 'fetch'],
+  },
+  {
+    breed: 'Husky',
+    averageWeight: 26,
+    activities: ['running', 'agility', 'swimming'],
+  },
+  {
+    breed: 'Bulldog',
+    averageWeight: 36,
+    activities: ['sleeping'],
+  },
+  {
+    breed: 'Poodle',
+    averageWeight: 18,
+    activities: ['agility', 'fetch'],
+  },
+];
+// 1. Store the the average weight of a "Husky" in a variable "huskyWeight"
+let huskyWeight = breeds.find(
+  element => element.breed === 'Husky'
+).averageWeight;
+console.log(huskyWeight);
+
+// 2. Find the name of the only breed that likes both "running" and "fetch" ("dogBothActivities" variable)
+let dogBothActivitie = breeds.find(
+  oggetto =>
+    oggetto.activities.includes('running') &&
+    oggetto.activities.includes('fetch')
+).breed;
+console.log(dogBothActivitie);
+
+// 3. Create an array "allActivities" of all the activities of all the dog breeds
+
+// 172 SORT
+// strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+
+// numbers
+console.log(movements);
+
+// return < 0 , A , B (keep order)
+// return > 0 , B , A (switch order)
+// Ascending
+movements.sort((a, b) => {
+  if (a > b) return 1;
+  if (a < b) return -1;
+});
+console.log(movements);
+
+// Descendinng
+movements.sort((a, b) => {
+  if (a > b) return -1;
+  if (a < b) return 1;
+});
+console.log(movements);
+
+movements.sort((a, b) => b - a);
+console.log(movements);
+
+const points = [40, 100, 1, 5, 25, 10];
+console.log(
+  points.sort(function (a, b) {
+    return a - b;
+    // if negative - a before b <--
+    // if positive +  b before a -->
+  })
+);
--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,153 @@
 'use strict';
+
+// creo un prototipo
+// ! constructor function
+
+const Person = function (FirstName, birthYear) {
+  this.FirstName = FirstName;
+  this.birthYear = birthYear;
+};
+// 2. creo Object linking to the instance prototype
+const Jonas = new Person('Jonas', 1991);
+console.log(Jonas);
+// 3. auth return what is inside of the contrucor function
+
+// 222 PROTOTYPES
+
+const matilda = new Person('Matilda', 2017);
+const jack = new Person('Jack', 1975);
+console.log(matilda, jack);
+
+console.log(Jonas instanceof Person);
+
+// prototypes
+console.log(Person);
+console.log(Person.prototype);
+
+// *aggiungo metodo al person prototype
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+
+Jonas.calcAge();
+matilda.calcAge();
+console.log(matilda);
+
+console.log(Jonas.__proto__);
+console.log(Jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(Jonas));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+
+Person.prototype.species = 'Homo Sapiens';
+console.log(Jonas.species, matilda.species);
+
+console.log(Jonas.hasOwnProperty('firstName'));
+console.log(Jonas.hasOwnProperty('species'));
+
+// 224 prototypal inheritance on built in objects
+console.log(Jonas);
+// object prototype - top of the prototyupe chain
+console.log(Jonas.__proto__);
+console.log(Jonas.__proto__.__proto__);
+console.log(Jonas.__proto__.__proto__.__proto__); // null
+
+console.dir(Person);
+console.dir(Person.prototype);
+
+const arr = [3, 6, 4, 5, 6, 9, 3];
+console.log(arr);
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+
+// ! car prototype CHALLENGE 1
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+};
+
+// car instance creation 2 cars
+const Ferrari = new Car('Ferrari', 300);
+const alfa = new Car('Alfa', 220);
+
+// aggiungo method to the car prototype
+Car.prototype.acceleration = function () {
+  this.speed += 10;
+  console.log(`${this.make} is going at ${this.speed}`);
+};
+
+//brake method
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.make} braking is going at ${this.speed}`);
+};
+
+// accedere alla funzionalita
+
+Ferrari.acceleration();
+Ferrari.acceleration();
+Ferrari.acceleration();
+Ferrari.brake();
+Ferrari.brake();
+Ferrari.brake();
+// ! ----
+
+// ! 226 ES6
+// CLASS DECLARATION
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+
+  // Metyods will be added to .prototype property
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+}
+
+const jessica = new PersonCl('Jessica', 1996);
+jessica.greet();
+
+// 227 setteers and getters
+// 228 static method
+//!  229 OBJECT.CREATE
+console.log(' ');
+console.log('----object create -----');
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+
+  init(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
+
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = 'Steven';
+steven.birthYear = 2002;
+steven.calcAge();
+console.log(steven);
+
+console.log(steven.__proto__ === PersonProto);
+const sarah = Object.create(PersonProto);
+sarah.init('Sarah', 1979);
+sarah.calcAge();
+
+// CHLLENGE 2
