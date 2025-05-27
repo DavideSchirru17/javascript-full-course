ad10a8f2491fb7513fad2f7ba8bcbf7e82ce22c8
DavideSchirru17 <schirrudavide@icloud.com>
2025-05-27 14:16:29 +0100

    265 chaning promises

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -10,5 +10,107 @@ const countriesContainer = document.querySelector('.countries');
 // https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lng}
 
 ///////////////////////////////////////
-https://countries-api-836d.onrender.com/countries/
+//countries-api-836d.onrender.com/countries/
 
+const renderCountry = function (data, className = '') {
+  const html = `
+          <article class="country ${className}">
+            <img class="country__img" src="${data.flag}" />
+            <div class="country__data">
+              <h3 class="country__name">${data.name}</h3>
+              <h4 class="country__region">${data.region}</h4>
+              <p class="country__row"><span>👫</span>${(
+                +data.population / 1000000
+              ).toFixed(1)}</p>
+              <p class="country__row"><span>🗣️</span>${
+                data.languages[0].name
+              }</p>
+              <p class="country__row"><span>💰</span>${
+                data.currencies[0].name
+              }</p>
+            </div>
+          </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+  // AJAX CALL1
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    // console.log(data);
+
+    // render country 1
+    renderCountry(data);
+
+    // get neighbour country 2
+    const [neighbour] = data.borders;
+    if (!neighbour) return;
+
+    // AJAX CALL COUNTRY 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v2/alpha/${neighbour}`);
+    request2.send();
+    // console.log(request2);
+
+    request2.addEventListener('load', function () {
+      const data2 = JSON.parse(this.responseText);
+      // console.log(data2);
+      renderCountry(data2, 'neighbour');
+    });
+  });
+};
+
+getCountryAndNeighbour('portugal');
+
+// get country datas from server
+// fetch .then  response.json()
+// then data > render in html
+// call getCountryData('Portugal ')
+// PRENDERE I DATI DAL SERVER , PROMISE JSON , RENDERING DEI DATI SU HTML
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(function (response) {
+      console.dir(response);
+      console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      renderCountry(data[0]);
+    });
+};
+getCountryData('portugal');
+
+// esiest
+const getCountryData2 = function (country) {
+  fetch(`https://restcountries.com/v2/name/${country}`).then(response =>
+    response.json().then(data => renderCountry(data[0]))
+  );
+};
+getCountryData2('italy');
+
+// 265 channing promoisese
+const getCountryData265 = function (country) {
+  // fetch req resp data from url
+  fetch(`https://restcountries.com/v2/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+
+      // chain more API call chainign - other country
+      return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
+    })
+    .then(response => response.json())
+    .then(data => renderCountry(data, 'neighbour'));
+};
+getCountryData265('france');
